daecb437f38f371b0f2c62312c8fcb987758156d
JArzolaUofH <justinearzola@yahoo.com>
2024-06-13 10:14:29 -0500

    finished

---
+++ script.js
@@ -0,0 +1,10 @@
+const { isAsyncFunction } = require("util/types");
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+
+}   
+
